22611e14b0a35ae59e5001426eed83a973a3fb31
lan7012 <545614720@qq.com>
2019-06-16 11:08:00 +0800

    Default Changelist

--- 练习.py
+++ 练习.py
@@ -1,13 +1,1 @@
-import os
-libs = {"numpy", "matplotlib", "pillow", "sklearn", "requests",\
-        "jieba", "beautifulsoup4", "wheel", "networkx", "sympy",\
-        "pyinstaller", "django", "flask", "werobot", "pyqt5",\
-        "pandas", "pyopeng1", "pypdf2", "docopt", "pygame"}
-
-try:
-    for lib in libs:
-        os.system("pip install "+ lib)
-    print("successful")
-
-except:
-    print("Failed")
+测试

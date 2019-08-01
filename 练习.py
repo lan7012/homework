88de0c77c1054ea8b45f5f5b5f1f8b7860fe7338
lan7012 <545614720@qq.com>
2019-08-01 09:37:26 +0800

    new

--- 练习.py
+++ 练习.py
@@ -1,10 +1,1 @@
-import random
-boy = 0
-girl = 0
-for a in range(1000):
-    i = random.randint(1, 2)
-    if i == 1:
-        boy += 1
-    else:
-        girl += 1
-print("男孩{}女孩{}".format(boy, girl))
+测试
